87be38fd81cdfefad97ae39160f05bba83b3ce61
Nikita Shirokov <braindefender@gmail.com>
2019-11-24 19:45:54 +0700

    added ui

---
+++ Assets/Scripts/Views/FlangView.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FlangView : MonoBehaviour
+{
+
+  public GameObject player1;
+  public GameObject player2;
+
+  void Start()
+  {
+    // player1 = GameObject.Find("player1").GetComponent<AbstractController>();
+    // player2 = GameObject.Find("player2").GetComponent<AbstractController>();
+    // GameController gameController = new GameController(player1, player2);
+    // FlangView fv = new FlangView();
+    // Debug.Log(fv.gameController.freeCommandors);
+  }
+
+  void Update()
+  {
+
+  }
+}
