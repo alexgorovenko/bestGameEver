77a803a15b85c5fa7984b747943ba6eceaea124e
Слава Журавлёв <slawajur@mail.ru>
2019-11-24 19:41:54 +0700

    inverse game<->controller dependency

--- Assets/Scripts/Presenter/Controllers/ComputerController.cs
+++ Assets/Scripts/Presenter/Controllers/ComputerController.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class ComputerController : MonoBehaviour
+public class ComputerController : AbstractController
 {
     // Start is called before the first frame update
     void Start()
