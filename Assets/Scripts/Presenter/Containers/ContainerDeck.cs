aa15a353ee0c1ae5b15139d65fc045daed2095dc
Слава Журавлёв <slawajur@mail.ru>
2019-12-12 16:51:39 +0700

    added deck container

---
+++ Assets/Scripts/Presenter/Containers/ContainerDeck.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+
+using UnityEngine;
+
+public class ContainerDeck : AbstractContainer
+{
+  private List<ICardContainerItem> mCards = new List<ICardContainerItem>();
+  public event EventHandler<CardContainerEventArgs> CardAdded;
+  public override void addCard(ICardContainerItem item)
+  {
+    mCards.Add(item);
+    item.onAdd();
+
+    if (CardAdded != null)
+    {
+      CardAdded(this, new CardContainerEventArgs(item));
+    }
+  }
+  public override void removeCard(ICardContainerItem item)
+  {
+    mCards.Remove(item);
+    item.onRemove();
+  }
+}
