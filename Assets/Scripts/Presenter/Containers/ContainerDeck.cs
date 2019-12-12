eb832014743ed3079069b17449a49e918e2594f0
Слава Журавлёв <slawajur@mail.ru>
2019-12-12 20:53:30 +0700

    added methods get card from deck

--- Assets/Scripts/Presenter/Containers/ContainerDeck.cs
+++ Assets/Scripts/Presenter/Containers/ContainerDeck.cs
@@ -6,4 +6,19 @@ using UnityEngine;
 
 public class ContainerDeck : AbstractContainer
 {
+  public ICardContainerItem GetCard()
+  {
+    ICardContainerItem card = this.mCards[0];
+    this.mCards.RemoveAt(0);
+    return card;
+  }
+
+  public List<ICardContainerItem GetCards(int number)
+  {
+    List<ICardContainerItem> cards = new List<ICardContainerItem>(number);
+    for (var i = 0; i < number; i++) {
+      cards.Add(this.GetCard());
+    }
+    return cards;
+  }
 }
