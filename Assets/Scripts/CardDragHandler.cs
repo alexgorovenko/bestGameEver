a5397c40fefe292a6ba252c37c00afc09240b568
Nikita Shirokov <braindefender@gmail.com>
2019-12-27 23:19:47 +0700

    new ui

--- Assets/Scripts/CardDragHandler.cs
+++ Assets/Scripts/CardDragHandler.cs
@@ -6,6 +6,7 @@ using UnityEngine.EventSystems;
 public class CardDragHandler : MonoBehaviour, IDragHandler, IBeginDragHandler, IEndDragHandler
 {
   [SerializeField] GameObject player;
+  [SerializeField] GameObject canvas;
   public Vector3 delta;
   private Vector3 initialPosition;
   public void OnBeginDrag(PointerEventData eventData)
@@ -15,8 +16,13 @@ public class CardDragHandler : MonoBehaviour, IDragHandler, IBeginDragHandler, I
   public void OnDrag(PointerEventData eventData)
   {
     player.GetComponent<PlayerController>().currentDraggableCard = gameObject.GetComponent<Card>();
-    float xT = eventData.position.x / Screen.width * 1280 - 640;
-    float yT = eventData.position.y / Screen.height * 720 - 360;
+    float cw = ((RectTransform)canvas.transform).rect.width;
+    float ch = ((RectTransform)canvas.transform).rect.height;
+    float scale = canvas.transform.localScale.x;
+    float pX = (eventData.position.x - 30.0f) / Screen.width;
+    float pY = 1.0f - (eventData.position.y - 45.0f) / Screen.height;
+    float xT = cw * (pX - 0.5f) * scale;
+    float yT = (-ch * (pY - 0.5f) * scale);
     float zT = 0;
     delta = new Vector3(xT, yT, zT);
     transform.position = delta;
