990649a0f5992232c3ae7a85aeabcd63ce4fac7c
Слава Журавлёв <slawajur@mail.ru>
2020-01-14 01:39:50 +0700

    beginning of menu
    https://trello.com/c/luAeCJzy

---
+++ Assets/Scripts/Menu/SceneWrapperButton.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class SceneWrapperButton : MonoBehaviour
+{
+    [SerializeField]
+    string scene;
+    // Start is called before the first frame update
+    void Start()
+    {
+        GetComponent<Button>().onClick.AddListener(Wrap);
+    }
+
+    void Wrap()
+    {
+        SceneManager.LoadScene(this.scene);
+    }
+}
